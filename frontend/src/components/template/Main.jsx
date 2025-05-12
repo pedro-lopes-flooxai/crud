36b723a3b21ca761fdf74a48dba94251a9e6e2f8
pedro-lopes-flooxai <pedro.lopes.flooxai@gmail.com>
2025-05-12 15:28:19 -0300

    add: main and header

--- frontend/src/components/template/Main.jsx
+++ frontend/src/components/template/Main.jsx
@@ -4,10 +4,11 @@ import Header from './Header'
 
 export default props =>
     <React.Fragment>
-        <Header>
-            <main className='content'>
-                Conteúdo
+        <Header {...props}></Header>
+            <main className='content container-fluid'>
+                <div className="p-3 mt-3">
+                    {props.children}
+                </div>
             </main>
-
-        </Header>
+    
     </React.Fragment>
--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -1,15 +1,20 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
 import './App.css'
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 
 import Logo from '../components/template/Logo'
 import Nav from '../components/template/Nav'
-import Main from '../components/template/Main'
+import Routes from './Routes'
 import Footer from '../components/template/Footer'
 
 export default props =>
-    <div className="app">
-        <Logo></Logo>
-        <Nav></Nav>
-        <Main></Main>
-        <Footer></Footer>
-    </div>
+    <BrowserRouter>
+        <div className="app">
+            <Logo></Logo>
+            <Nav></Nav>
+            <Routes></Routes>
+            <Footer></Footer>
+        </div>
+    </BrowserRouter>
